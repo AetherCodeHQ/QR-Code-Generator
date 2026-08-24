dc0a40d38ae32faf9d38d2158f913dcd54a074b1
AetherCodeHQ <noreply@users.noreply.github.com>
2026-08-24 15:46:39 +0300

    v2.1.0: unique implementation replaces generic stub

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -9,32 +8,73 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("usage: QR-Code-Generator <bytes|lines|reverse|upper|lower>")
+		fmt.Println("usage: qr-code-generator <text> [out.svg]")
 		os.Exit(1)
 	}
-	mode := os.Args[1]
-	in := ""
+	grid := buildGrid(os.Args[1])
+	svg := renderSVG(grid)
+	w := os.Stdout
 	if len(os.Args) > 2 {
-		in = os.Args[2]
-	}
-	lines := strings.Split(in, "\n")
-	switch mode {
-	case "lines":
-		fmt.Println(len(lines))
-	case "bytes":
-		fmt.Println(len(in))
-	case "upper":
-		fmt.Println(strings.ToUpper(in))
-	case "lower":
-		fmt.Println(strings.ToLower(in))
-	case "reverse":
-		runes := []rune(in)
-		for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-			runes[i], runes[j] = runes[j], runes[i]
+		f, err := os.Create(os.Args[2])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
 		}
-		fmt.Println(string(runes))
-	default:
-		fmt.Fprintln(os.Stderr, "unknown mode:", mode)
-		os.Exit(1)
+		defer f.Close()
+		w = f
 	}
+	w.WriteString(svg)
+	fmt.Fprintf(os.Stderr, "%dx%d grid\n", len(grid), len(grid))
+}
+
+func buildGrid(text string) [][]bool {
+	g := make([][]bool, 21)
+	for i := range g {
+		g[i] = make([]bool, 21)
+	}
+	drawFinder(g, 0, 0)
+	drawFinder(g, 14, 0)
+	drawFinder(g, 0, 14)
+	pos := 0
+	for _, ch := range []byte(text) {
+		for bit := 7; bit >= 0; bit-- {
+			set := (ch>>uint(bit))&1 == 1
+			r := pos / 19
+			c := pos % 19
+			if r < 21 && c+1 < 21 {
+				g[r][c+1] = set
+			}
+			pos++
+		}
+	}
+	return g
+}
+
+func drawFinder(g [][]bool, r, c int) {
+	for dr := 0; dr < 7; dr++ {
+		for dc := 0; dc < 7; dc++ {
+			if r+dr < 21 && c+dc < 21 {
+				border := dr == 0 || dr == 6 || dc == 0 || dc == 6
+				inner := dr >= 2 && dr <= 4 && dc >= 2 && dc <= 4
+				g[r+dr][c+dc] = border || inner
+			}
+		}
+	}
+}
+
+func renderSVG(g [][]bool) string {
+	var b strings.Builder
+	n := len(g)
+	sz := 8
+	fmt.Fprintf(&b, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%d\" height=\"%d\">\n", n*sz, n*sz)
+	b.WriteString("<rect width=\"100%\" height=\"100%\" fill=\"white\"/>\n")
+	for r, row := range g {
+		for c, v := range row {
+			if v {
+				fmt.Fprintf(&b, "<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" fill=\"black\"/>\n", c*sz, r*sz, sz, sz)
+			}
+		}
+	}
+	b.WriteString("</svg>\n")
+	return b.String()
 }
